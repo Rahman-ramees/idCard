09648d4d43d280b69dd0c4ac41f1687c1d7654f3
NETCOM <rahmanramees@protracked.in>
2025-07-26 18:14:43 +0530

    updated pdf

--- send.php
+++ send.php
@@ -65,7 +65,7 @@ $html = '
 <html>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <link href="http://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     <style>
         @page {
             size: 55mm 85mm;
