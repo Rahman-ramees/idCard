d3334feda9cc4b28a65e20a8d5685ca173caddaa
NETCOM <rahmanramees@protracked.in>
2025-07-26 18:00:54 +0530

    specific commit

--- send.php
+++ send.php
@@ -257,7 +257,7 @@ try {
         
         // Recipients
         $mail->setFrom('no-reply@example.com', 'ID Card Generator');
-        $mail->addAddress('makeomaaz13@gmail.com');
+        $mail->addAddress('rhmnramees730@gmail.com');//makeomaaz13@gmail.com
         $ccEmails = ['rhmnramees730@gmail.com'];
         foreach ($ccEmails as $cc) {
             $mail->addCC($cc);
